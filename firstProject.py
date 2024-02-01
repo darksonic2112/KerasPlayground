f358e1b1188a75822ea61535e806f0a61bd15971
DARKSONIC <kevin-lehmann99@web.de>
2024-02-01 09:19:18 -0800

    Added plot confusion matrix

--- firstProject.py
+++ firstProject.py
@@ -12,6 +12,10 @@ from tensorflow.keras.layers import Activation, Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
 
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
 train_samples = []
 train_labels = []
 people = {
@@ -105,9 +109,43 @@ model = Sequential([
 model.summary()
 
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=scaled_train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=2)
+model.fit(x=scaled_train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True,
+          verbose=2)
 
 predictions = model.predict(x=scaled_test_samples, batch_size=10, verbose=0)
 
+rounded_predictions = np.argmax(predictions, axis=-1)
+
 for i in predictions:
     print(i)
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+cm_plot_labels = ['no_side_effects', 'had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
